2adf107ee6783932f0630cca68f5db6d9a93a5de
Zikzik <34153457+zekron@users.noreply.github.com>
2021-06-30 15:46:01 +0800

    Update Scene
    Add some button callbacks;

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -6,7 +6,8 @@ public class Client : MonoBehaviour
 {
     FurnitureFactory _victorianFactory = new VictorianFurnitureFactory();
     FurnitureFactory _modernFactory = new ModernFurnitureFactory();
-    // Start is called before the first frame update
+    FurnitureFactory _clientFactory;
+
     void Start()
     {
     }
@@ -31,5 +32,40 @@ public class Client : MonoBehaviour
             coffeeTable.HaveALook();
             Debug.Log($"The price is {coffeeTable.GetPrice():C}");
         }
+    }
+
+    public void UseModernFactory()
+    {
+        _clientFactory = _modernFactory;
+    }
+
+    public void UseVictorianFactory()
+    {
+        _clientFactory = _victorianFactory;
+    }
+
+    public void CreateSofa()
+    {
+        if (_clientFactory == null) return;
+
+        Sofa sofa = _clientFactory.CreateProduct<Sofa>(FutnitureType.Sofa);
+        sofa.Sit();
+        Debug.Log($"The price is {sofa.GetPrice():C}");
+    }
+    public void CreateCoffeeTable()
+    {
+        if (_clientFactory == null) return;
+
+        CoffeeTable coffeeTable = _clientFactory.CreateProduct<CoffeeTable>(FutnitureType.CoffeeTable);
+        coffeeTable.HaveALook();
+        Debug.Log($"The price is {coffeeTable.GetPrice():C}");
+    }
+    public void CreateChair()
+    {
+        if (_clientFactory == null) return;
+
+        Chair chair = _clientFactory.CreateProduct<Chair>(FutnitureType.Chair);
+        chair.Sit();
+        Debug.Log($"The price is {chair.GetPrice():C}");
     }
 }
